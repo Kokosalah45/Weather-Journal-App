08921ad9b7b521c68330ee0e10ff1591ff2739e9
Kokosalah45 <kokp.amged@gmail.com>
2022-09-01 15:12:54 +0200

    aded path join to generalize path for hostin

--- server.js
+++ server.js
@@ -1,33 +1,28 @@
 const projectData = {};
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const port =   3000 || process.env.PORT;
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const port = 3000 || process.env.PORT;
 const app = express();
 const jsonParser = bodyParser.json();
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.use(express.static('website'));
+app.use(express.static(path.join(__dirname, "/website")));
 app.use(cors());
 app.use(jsonParser);
 
+app.post("/insertdata", urlencodedParser, function (req, res, next) {
+  if (req.body) {
+    projectData = req.body;
+    res.send("done !");
+  }
+  next();
+});
+app.get("/getmostrecentdata", urlencodedParser, function (req, res, next) {
+  res.send(projectData);
+});
 
-
-
-app.post('/insertdata', urlencodedParser, function (req, res,next) {
-    
-    if(req.body){ 
-        projectData = req.body;
-    res.send('done !');
- }
- next();
-})
-    app.get('/getmostrecentdata' , urlencodedParser, function (req, res, next) {
-        res.send(projectData);
-    })
-
-
-app.listen(port , ()=>{
-    console.log("server is listening !");
+app.listen(port, () => {
+  console.log("server is listening !");
 });
